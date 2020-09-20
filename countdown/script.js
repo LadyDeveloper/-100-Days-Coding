96daa536923acc8600ce28605b2b07cbbe27bcfb
LadyDeveloper <anhacorrea@hotmail.com>
2020-09-20 18:15:34 -0500

    Final version Count Down page

--- countdown/script.js
+++ countdown/script.js
@@ -1,11 +1,17 @@
-function countDown() {
-  const date = new Date();
-  const today = new Date().toDateString();
-  const days = date.getDate();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
+const target = new Date("Dec 29, 2020").getTime();
 
+function countDown() {
+  const now = new Date().getTime();
+  const diff = target - now;
+
+  const today = new Date().toDateString();
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  document.querySelector(".date").innerHTML = today;
   document.querySelector("#days").innerHTML = days;
   document.querySelector("#hours").innerHTML = hours;
   document.querySelector("#minutes").innerHTML = minutes;
